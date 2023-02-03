035bd717293b9aee2fe8bfbb9a46653dda543a8c
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-03 18:14:17 -0500

    navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+
+                <Link className='logo-container' to='/'>
+                    <div>Logo</div>
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        Shop
+                    </Link>
+                </div>
+                <h1>I am the navigation bar.</h1>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;
