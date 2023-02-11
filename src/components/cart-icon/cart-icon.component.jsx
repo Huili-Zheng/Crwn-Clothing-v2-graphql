f2c7eb3876953493c0bb413fd6ff82b8d55c3593
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-10 22:21:22 -0500

    cart-icon styles

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,11 @@
-import "./cart-icon.styles.scss";
+import "./cart-icon.styles.jsx";
 import { useContext } from "react";
 import { CartContext } from "../../context/card.context";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import {
+  ShoppingIcon,
+  CartIconContainer,
+  ItemCount,
+} from "./cart-icon.styles.jsx";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cardCount } = useContext(CartContext);
@@ -9,10 +13,10 @@ const CartIcon = () => {
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+    <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cardCount}</span>
-    </div>
+      <ItemCount>{cardCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
