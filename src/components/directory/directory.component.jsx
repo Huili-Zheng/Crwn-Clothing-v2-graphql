49138a783a40942aa0ec955c693ce24a927db13a
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-11 00:19:56 -0500

    upload the category description to db

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,47 @@
 import DirectoryItem from "../directory-item/directory-item.component";
 import { DirectoryContainer } from "./directory.styles";
+import { useEffect } from "react";
+import { addCollectionAndDocuments } from "../../utils/firebase/firebase.utils";
 
-const Directory = ({ categories }) => (
-  <DirectoryContainer>
-    {categories.map((category) => (
-      <DirectoryItem key={category.id} category={category} />
-    ))}
-  </DirectoryContainer>
-);
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+
+const Directory = () => {
+  useEffect(() => {
+    addCollectionAndDocuments("categoryDescription", categories);
+  }, []);
+  return (
+    <DirectoryContainer>
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </DirectoryContainer>
+  );
+};
 
 export default Directory;
